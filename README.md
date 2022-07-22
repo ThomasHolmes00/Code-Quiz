615ae330aeaae885862eba495ea04ff8a507a6db
ThomasHolmes00 <thomas.scott.holmes00@gmail.com>
2022-07-21 18:28:55 -0600

    Code Quiz Files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Code-Quiz
+
+## Description
+Below is a Code Quiz that will test your skills on coding. You have have 60 seconds to get through the 6 questions provided. Click the start button to begin the Quiz. Every incorrect answer will deduct time. The remaining time will be your final score. Good luck!
+
+<br></br>
+
+## Screenshot
+
+<br></br>
+
+## Website
---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Js for timer //
+var timer = document.getElementById("time");
+var timeLeft = 90;
+
+        if (timeLeft <= 0) { 
+            clearInterval(downloadTimer);
+        
+        } (1000); 
+
+
+
+var firstQuery = document.querySelector("firstquery");
+var secondQuery = document.querySelector("secondquery");
+var thirdQuery = document.querySelector("thirdquery");
+var fourthQuery = document.querySelector("fourthquery");
+var fifthQuery = document.querySelector("fifthquery");
+var sixthQuery = document.querySelector("sixthquery");
+
+document.getElementById
